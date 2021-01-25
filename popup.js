761334d4ab17d3ddfa8d750f72561d53fb5f8ac9
fluffyhake <haakon.reppen@gmail.com>
2021-01-25 23:07:12 +0100

    Toggle button talks with the background script

--- popup.js
+++ popup.js
@@ -1,14 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.querySelector("button").addEventListener("click", onclick, false)
 
-    function onclick (){
-        chrome.tabs.query({currentWindow: true, active: true},
-            function (tabs){
-                chrome.tabs.sendMessage(tabs[0].id, "hi")
-            })
-    }
-    
+     var checkbox = document.querySelector("input[id=enabler-slider]");
+     //const port = chrome.runtime.connect({ name: 'poppingport' });
+
+     checkbox.addEventListener('change', function() {
+         if (this.checked) {
+             var message = "Checked"
+             console.log(message);
+      } else {
+        var message = "Unchecked"
+        console.log(message);
+      }
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {greeting: message}, function(response) {
+            console.log(response.ack)})});
+     })
+  
+
 
 },false)
-
-var enableSliderToggle = document.querySelectorAll("enabler-slider");
--- uber-replacer.js
+++ uber-replacer.js
@@ -1,17 +1,21 @@
-
-var image = document.querySelectorAll("img");
-var l = image.length
+// var image = document.querySelectorAll("img");
+// var l = image.length
 
 var poppingGif = chrome.runtime.getURL("popping.gif");
-console.log(poppingGif)
+// console.log(poppingGif)
 
-for (let i = 0; i < l; i++) {
-  image[i].src= poppingGif
-  image[i].srcset= poppingGif
-}
-
+// for (let i = 0; i < l; i++) {
+//   image[i].src= poppingGif
+//   image[i].srcset= poppingGif
+// }
 console.log("POPPING PLUGIN INITIATED");
 function replacepop() {
+  if (activate = null && activate != False) {
+    activate = True
+  }
+  if (activate = true) {
+    
+ 
     var image = document.querySelectorAll("img")
     var l = image.length
     console.log("REPLACINGGGGG")
@@ -19,14 +23,40 @@ function replacepop() {
         image[i].src= poppingGif
         image[i].srcset= poppingGif
     }
-}
+}else{
+  console.log("Not activated yet!")
+}}
 
 var enabled = document.querySelectorAll("enabler")
 
-chrome.runtime.onMessage.addListener(function (request){
-  var 
-})
+// chrome.runtime.onMessage.addListener(request,sender, sendResponse => {
+//   console.log(request.sender)
+//   sendResponse({ recieved: "Messege Recieved "+ request.greeting})
+//   if (request.greeting === 'Checked') {
+//       console.log('received a Checked message');
+
+//   }else if (request.greeting === "Unchecked"){ 
+//       console.log('recieved a Unchecked messege')
+
+//     }
+//   }
+//   )
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if (request.greeting == "Checked"){
+      sendResponse({ack: "recieved "+request.greeting});
+      var activate = "True"
+  }else if (request.greeting == "Unchecked") {
+      sendResponse({ack: "recieved "+request.greeting})
+      var activate = "False"
+      
+  }}
+)
 
 
 
-setInterval("replacepop()",500);
+
+setInterval("replacepop()",500)
