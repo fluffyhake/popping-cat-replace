d9084e8b92c75362bc106212a0071e21f19d6210
fluffyhake <haakon.reppen@gmail.com>
2021-02-27 13:03:05 +0100

    The replacer still works, but the buttons are now mega borked

--- popup.js
+++ popup.js
@@ -16,6 +16,34 @@ document.addEventListener('DOMContentLoaded', function () {
             console.log(response.ack)})});
      })
   
+// TESTING THE NEW BUTTON!
+      $(function(){
+        //default value is "start"
+        var currentState = localStorage.currentState || "start";
+        //cache button DOM element reference
+        var $toggleBtn = $("#toggle-btn");
+
+        //update button status
+        if(currentState==="stop"){
+            $toggleBtn.text("OFF");
+        }
+
+        //register button click handler
+        $toggleBtn.click(function(){
+            if(currentState==="start"){
+                $toggleBtn.text("OFF");
+                localStorage.currentState="stop";
+            }
+            if(currentState==="stop"){
+                $toggleBtn.text("ON");
+                localStorage.currentState="start";
+            }
+        });
+      });
+
+
 
 
 },false)
+
+
